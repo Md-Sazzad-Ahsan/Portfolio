9d4681c56358fb684877edc5646711ef01a72ea7
Md. Sazzad Ahsan <himu200037@diit.edu.bd>
2024-08-14 17:57:56 +0600

    portfolio directory renamed for case sensitivity

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 export const metadata: Metadata = {
   title: "Md. Sazzad Ahsan",
-  description: "Programmer Developer Software-Engineer Computer Engineer",
+  description: "Developer Computer-Engineer Programmer Content-Writer",
 };
 
 
